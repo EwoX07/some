8dd0cc953a350e9b6cced2820e2fef20f400bd9d
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-07 15:43:58 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -6,13 +6,16 @@ module.exports = async (client, oldState, newState) => {
     const master = await client.guilds.cache.get(guild).channels.cache.get(channel);
     if (!master) return;
 
+    let clone;
+
     if (oldState.channel !== newState.channel) {
 
         if (newState.channelID === master.id) {
-            const clone = await master.clone({ name: newState.member.user.username }).catch(() => {});
-            await newState.setChannel(clone).then(c => {
-                if (c.members.filter(m => !m.user.bot).size < 1) return c.delete();
-            }).catch(() => {});
+            clone = await master.clone({ name: newState.member.user.username }).catch(() => {});
+            await newState.setChannel(clone).catch(() => {});
+        }
+        if (clone.members.filter(m => !m.user.bot).size < 1) {
+            return clone.delete();
         }
     }
 };
