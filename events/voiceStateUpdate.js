b365f1728ddf629e69308b5290f270e3362af2fd
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-07 15:01:21 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,9 +3,12 @@ module.exports = async (oldState, newState) => {
     const guild = "736362962566250508";
     const channel = "754148912922624021";
 
-    if (oldState !== newState) {
+    const master = await client.guilds.cache.get(guild).channels.cache.get(channel);
+    if (!master) return;
 
-        if (newState.channelID === channel) console.log("join");
+    if (oldState.channel !== newState.channel) {
+
+        if (newState.channelID === master.id) console.log("join");
     }
 
-}
+};
