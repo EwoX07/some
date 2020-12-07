8076d5bb646c877ea7527db6144869d128f01eea
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-07 13:19:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
-const exConfig = require("./config.json");
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
-const DisTube = require("distube");
  
 //const { ReactionRoleManager } = require("discord.js-collector");
 
@@ -18,11 +16,9 @@ const logs = require("discord-logs");
 logs(client);
 
 client.config = require("./config.js");
-client.distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true, leaveOnFinish: true });
 client.logger = require("./modules/logger.js");
 require("./modules/functions.js")(client);
 
-client.emotes = exConfig.emoji;
 client.commands = new Enmap();
 client.aliases = new Enmap();
 client.settings = new Enmap({
@@ -50,36 +46,6 @@ reactionRoleManager.on('reactionRoleRemove', (member, role) => {
     console.log(member.displayName + ' lose the role' + role.name)
 });*/
 
-const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
-client.distube
-    .on("playSong", (message, queue, song) => message.channel.send(
-        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${message.author.username}\n${status(queue)}`
-    ))
-    .on("addSong", (message, queue, song) => message.channel.send(
-        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${message.author.username}`
-    ))
-    .on("playList", (message, queue, playlist, song) => message.channel.send(
-        `${client.emotes.play} | Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${message.author.username}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
-    ))
-    .on("addList", (message, queue, playlist) => message.channel.send(
-        `${client.emotes.success} | Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`
-    ))
-    .on("initQueue", queue => {
-    queue.autoplay = false;
-    queue.volume = 100;
-})
-    .on("searchResult", (message, result) => {
-        let i = 0;
-        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`).then(
-          message => {message.delete({ timeout: 60000 })
-                     })
-    })
-    .on("searchCancel", (message) => message.channel.send(`${client.emotes.error} | Searching canceled`))
-    .on("noRelated", message => message.channel.send(">>> Can't find related video to play. Stop playing music."))
-    .on("finish", message => message.channel.send(">>> No more song in queue"))
-    .on("empty", message => message.channel.send(">>> Channel is empty. Leaving the channel"))
-    .on("error", (message, err) => message.channel.send(`${client.emotes.error} | An error encountered: ${err}`));
-
 const init = async () => {
   const cmdFiles = await readdir("./commands/");
   client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
@@ -103,9 +69,8 @@ const init = async () => {
     const thisLevel = client.config.permLevels[i];
     client.levelCache[thisLevel.name] = thisLevel.level;
   }
-  //client.login(client.exConfig.TOKEN);
 };
 
 init();
 
-client.login(process.env.TOKEN);
+client.login(client.config.token);
